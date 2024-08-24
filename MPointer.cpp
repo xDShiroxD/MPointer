1e986a3d0e1111c020c437f236202b39ad085ca6
xDShiroxD <bryan.garro1321@gmail.com>
2024-08-24 10:28:17 -0500

    Operator =, con pruebas del mismo

--- MPointer.cpp
+++ MPointer.cpp
@@ -4,8 +4,15 @@ template <typename T>
 class MPointer {
     private:
         T* ptr;
+        int ID; 
 
-        MPointer(T* p) : ptr(p) {} //Para que solo sea llamado con New()
+        MPointer(T* p) : ptr(p), ID(generadorID()) {} //Para que solo sea llamado con New()
+
+    //Generador de ID's unicos
+    int generadorID() {
+        static int id = 0;
+        return ++id;
+    }
 
     public:
 
@@ -24,19 +31,35 @@ class MPointer {
             return *ptr;
         }
 
-        T operator&() const {
-            return *ptr;
+        T* operator&() const {
+            return ptr;
+        }
+
+        MPointer& operator=(const MPointer& other) {
+            if (this != static_cast<const void*>(&other)) {
+                this->ptr = other.ptr;
+                this->ID = other.ID;
+
+                //Aqui se actualiza el GC
+
+            }
+            return *this;
         }
 
 };
 
 int main() {
-    MPointer<int> myPtr = MPointer<int>::New();  // Crea un MPointer<int> y reserva memoria para un int
+    MPointer<int> myPtr = MPointer<int>::New();  
+    MPointer<int> myPtr2 = MPointer<int>::New(); 
 
-    *myPtr = 5;  // Almacena el valor 5 en el entero encapsulado
+    *myPtr = 5; 
 
-    int valor = &myPtr;  // Obtiene el valor almacenado en myPtr
+    int valor = *myPtr;  
     std::cout << "Valor obtenido: " << valor << std::endl;
 
+    myPtr2 = myPtr;  // Copia el puntero y el ID de myPtr a myPtr2
+    std::cout << "Valor obtenido después de la asignación: " << *myPtr2 << std::endl;
+    std::cout << "Direccion uno:" << &myPtr << std::endl;
+    std::cout << "Direccion uno:" << &myPtr2 << std::endl;
     return 0;
 }
